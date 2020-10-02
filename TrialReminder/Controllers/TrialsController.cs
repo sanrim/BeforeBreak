9dca34d8338a14bda27cf87541c548bfe7d331cd
sanrim <30580639+sanrim@users.noreply.github.com>
2020-10-02 11:10:24 -0400

    Initial commit

---
+++ TrialReminder/Controllers/TrialsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrialReminder.Models;
+
+namespace TrialReminder.Controllers
+{
+    public class TrialsController : Controller
+    {
+        private readonly TrialsDataContext _context;
+
+        public TrialsController(TrialsDataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var results = await _context.Trials.ToListAsync();
+            return View(results);
+        }
+    }
+}
